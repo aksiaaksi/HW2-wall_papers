f143f1f596a14f1949ee9755021af15ab39afc9c
aksi <a.k.sattarov@gmail.com>
2019-02-09 14:50:10 +0300

    renamed 'f' to 'safety_factor'

--- calculate_of_wallpaper.py
+++ calculate_of_wallpaper.py
@@ -10,10 +10,10 @@ def calculate(lenght_room,wigth_room,lenght_wallpaper,wight_wallpaper,lenght_rap
     >>> calculate(2,2,10,1.06,0.32,2)#doctest:+ELLIPSIS
     1.82...
     """
-    f = 0.1 #  safety factor
+    safety_factor = 0.1 #  safety factor
     perimetr = (lenght_room + wigth_room) * 2
 
-    wallpaper_stripes = lenght_wallpaper / (lenght_raport + room_height + f )
+    wallpaper_stripes = lenght_wallpaper / (lenght_raport + room_height + safety_factor )
 
     number_rolls = (perimetr / wight_wallpaper) / wallpaper_stripes
 
